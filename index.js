ba2f4843fab318d6becc963efa81d5e61df7bffd
mstn <mstn@git.com>
2015-02-17 12:56:50 +0100

    refactor with OOP style

--- index.js
+++ index.js
@@ -5,43 +5,41 @@ var Promise = require('es6-promise').Promise;
 var csv = require('csvjson');
 var converter = require('json-2-csv');
 
-var config = {
-  data: {
-    masters:{
-      format:'json',
-      ext:'.json',
-      dir:'./cache/'
-    },
-    nodes:{
-      format:'json',
-      ext:'.json',
-      dir:'./cache/'
-    },
-    routes:{
-      format:'json',
-      ext:'.json',
-      dir:'./cache/'
-    },
-    stops:{
-      format:'json',
-      ext:'.json',
-      dir:'./cache/'
-    },
-    ways:{
-      format:'json',
-      ext:'.json',
-      dir:'./cache/'
-    },
-    agency:{
-      format:'csv',
-      ext:'.txt',
-      dir:'./gtfs/'
-    },
-    trips:{
-      format:'csv',
-      ext:'.txt',
-      dir:'./gtfs/'
-    }
+var defaultData = {
+  masters:{
+    format:'json',
+    ext:'.json',
+    dir:'./cache/'
+  },
+  nodes:{
+    format:'json',
+    ext:'.json',
+    dir:'./cache/'
+  },
+  routes:{
+    format:'json',
+    ext:'.json',
+    dir:'./cache/'
+  },
+  stops:{
+    format:'json',
+    ext:'.json',
+    dir:'./cache/'
+  },
+  ways:{
+    format:'json',
+    ext:'.json',
+    dir:'./cache/'
+  },
+  agency:{
+    format:'csv',
+    ext:'.txt',
+    dir:'./gtfs/'
+  },
+  trips:{
+    format:'csv',
+    ext:'.txt',
+    dir:'./gtfs/'
   }
 };
 
@@ -54,124 +52,116 @@ var parser = {
   }
 };
 
-
-function setConfig(newConfiguration){
-  _.extend(config.data, newConfiguration);
+var GtfsMaker = function(config){
+  config = config || {};
+  this._settings = config.settings;
+  this._data = _.extend(defaultData, config.data);
+  this.builders = {
+    routes: require('./builders/routes').bind(this),
+    shapes: require('./builders/shapes').bind(this),
+    stops: require('./builders/stops').bind(this),
+    stopTimes: require('./builders/stop_times').bind(this)
+  };
 };
 
-// TODO allow basedir override
-/**
- * load static assets from files
- *
- * list, list of file kind as specified in config file
- *
- * a file kind could be a simple file (csv or json)
- * or a directory of similar files
- *
- */
-function loadData(list){
-  return list.map(function(type){
+_.extend( GtfsMaker.prototype, {
+  cache:function cache(){
+    var OSM3S_API = 'http://api.openstreetmap.fr/oapi/interpreter';
 
-    var file = config.data[type];
-    var rootdir;
-    var filepath;
+    var mastersQuery = fs.readFileSync( __dirname + '/queries/get-master-routes.osm3s').toString();
+    var stopsQuery = fs.readFileSync(__dirname + '/queries/get-stops-nodes.osm3s').toString();
+    var routesQuery = fs.readFileSync(__dirname + '/queries/get-routes-rel.osm3s').toString();
+    var waysQuery = fs.readFileSync(__dirname + '/queries/get-routes-ways.osm3s').toString();
+    var nodesQuery = fs.readFileSync(__dirname + '/queries/get-routes-nodes.osm3s').toString();
 
-    if (!file){
-      throw new Error('No file config for type ' + type + '.');
+    var queries = [mastersQuery, stopsQuery, routesQuery, waysQuery, nodesQuery];
+
+    /*
+     * gets data from OSM Overpass API
+     */
+    function queryOSM(query){
+      return new Promise(function(resolve, reject){
+        request.get(OSM3S_API)
+          .query({data: query})
+          .end(function(err, res){
+            if(err){
+              reject(err);
+            } else {
+              resolve(res.body);
+            }
+          });
+      });
     }
-    // if it is a directory load all files in a bundle
-    if ( file.isDirectory ){
-      return fs.readdirSync( file.dir )
-        .map(function(filename){
-          filepath = file.dir + filename;
-          return {
-            name:filename,
-            content:parser[ file.format ].call(undefined, filepath )
-          };
-       }).map( file.transform );
-    } else {
-      filepath = file.dir + type + file.ext;
-      if ( !fs.existsSync(filepath)){
-        throw new Error('File not found ' + filepath + '.');
-      }
-      return parser[ file.format ].call(undefined, filepath );
+
+    /*
+     * array of arrays with [routes, ways, nodes]
+     */
+    function saveData(data){
+      var types = ['masters', 'stops', 'routes', 'ways', 'nodes'];
+      _.each(data, function(arr, index){
+        fs.writeFileSync('./cache/' + types[index] + '.json', JSON.stringify(arr));
+      });
+      console.log('data saved to ./cache');
     }
-  });
-}
 
-function saveDataAsCsv(data, filepath){
-  return new Promise(function(resolve, reject){
-    converter.json2csv(data, function(err, csv){
-      if (err){
-        reject(err);
-      } else {
-        fs.writeFileSync( filepath, csv );
-        resolve();
-      }
-    });
-  });
-}
+    /*
+     * get all data and save to disk
+     */
+    return Promise.all(queries.map(queryOSM))
+      .then(saveData);
 
-// XXX refactor, duplicate code also present in Gruntfile.js
-/**
- * params object with customization options for OSM queries
- */
-function cache(params){
-  var OSM3S_API = 'http://api.openstreetmap.fr/oapi/interpreter';
-
-  var mastersQuery = fs.readFileSync( __dirname + '/queries/get-master-routes.osm3s').toString();
-  var stopsQuery = fs.readFileSync(__dirname + '/queries/get-stops-nodes.osm3s').toString();
-  var routesQuery = fs.readFileSync(__dirname + '/queries/get-routes-rel.osm3s').toString();
-  var waysQuery = fs.readFileSync(__dirname + '/queries/get-routes-ways.osm3s').toString();
-  var nodesQuery = fs.readFileSync(__dirname + '/queries/get-routes-nodes.osm3s').toString();
-
-  var queries = [mastersQuery, stopsQuery, routesQuery, waysQuery, nodesQuery];
-
-  /*
-   * gets data from OSM Overpass API
-   */
-  function queryOSM(query){
-    return new Promise(function(resolve, reject){
-      request.get(OSM3S_API)
-        .query({data: query})
-        .end(function(err, res){
-          if(err){
-            reject(err);
-          } else {
-            resolve(res.body);
-          }
-        });
-    });
-  }
-
-  /*
-   * array of arrays with [routes, ways, nodes]
-   */
-  function saveData(data){
-    var types = ['masters', 'stops', 'routes', 'ways', 'nodes'];
-    _.each(data, function(arr, index){
-      fs.writeFileSync('./cache/' + types[index] + '.json', JSON.stringify(arr));
-    });
-    console.log('data saved to ./cache');
-  }
-
-  /*
-   * get all data and save to disk
-   */
-  return Promise.all(queries.map(queryOSM))
-    .then(saveData);
-
-};
-
-module.exports = {
-  builders: {
-    routes: require('./builders/routes'),
-    shapes: require('./builders/shapes'),
-    stops: require('./builders/stops'),
-    stopTimes: require('./builders/stop_times')
   },
-  cache:cache,
-  load:loadData,
-  saveAsCsv:saveDataAsCsv,
-  config:setConfig
-};
+  /**
+   * load static assets from files
+   *
+   * list, list of file kind as specified in config file
+   *
+   * a file kind could be a simple file (csv or json)
+   * or a directory of similar files
+   *
+   */
+  loadData: function loadData(list){
+    var data = this._data;
+    return list.map(function(type){
+
+      var file = data[type];
+      var rootdir;
+      var filepath;
+
+      if (!file){
+        throw new Error('No file config for type ' + type + '.');
+      }
+      // if it is a directory load all files in a bundle
+      if ( file.isDirectory ){
+        return fs.readdirSync( file.dir )
+          .map(function(filename){
+            filepath = file.dir + filename;
+            return {
+              name:filename,
+              content:parser[ file.format ].call(undefined, filepath )
+            };
+         }).map( file.transform );
+      } else {
+        filepath = file.dir + type + file.ext;
+        if ( !fs.existsSync(filepath)){
+          throw new Error('File not found ' + filepath + '.');
+        }
+        return parser[ file.format ].call(undefined, filepath );
+      }
+    });
+  },
+  saveDataAsCsv:function saveDataAsCsv(data, filepath){
+    return new Promise(function(resolve, reject){
+      converter.json2csv(data, function(err, csv){
+        if (err){
+          reject(err);
+        } else {
+          fs.writeFileSync( filepath, csv );
+          resolve();
+        }
+      });
+    });
+  }
+});
+
+module.exports = GtfsMaker;
