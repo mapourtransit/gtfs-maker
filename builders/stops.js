8e3db5faf1c77c4cbea144d14924841637a9456b
mstn <mstn@git.com>
2015-02-18 14:27:18 +0100

    changed return value for stop builder: use objects instead of anonymous lists

--- builders/stops.js
+++ builders/stops.js
@@ -9,14 +9,14 @@ module.exports = function(data){
   _.each(data[0].elements, function(element){
     if (element.type == 'node' ){
       if (element.tags.public_transport && element.tags.public_transport == 'platform'){
-        stops.push([
-          element.id, // stop_id
-          element.tags.ref || element.tags.rel, // stop_code
-          element.tags.name || element.tags['addr:street'], // stop_name
-          element.tags.desc, // stop_desc
-          element.lat, // stop_lat
-          element.lon // stop_lon
-        ]);
+        stops.push({
+          stop_id:element.id,
+          stop_code:element.tags.ref || element.tags.rel,
+          stop_name:element.tags.name || element.tags['addr:street'],
+          stop_desc:element.tags.desc,
+          stop_lat:element.lat,
+          stop_lon:element.lon
+        });
       }
     }
   });
